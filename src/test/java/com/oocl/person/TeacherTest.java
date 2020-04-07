3b1e75259031a0af67af96ca3d3462f4117d4086
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-07 15:05:51 +0800

    create unit test for Teacher Class

--- src/test/java/com/oocl/person/TeacherTest.java
+++ src/test/java/com/oocl/person/TeacherTest.java
@@ -7,6 +7,11 @@ import static org.junit.Assert.*;
 public class TeacherTest {
 
     @Test
-    public void introduce() {
+    public void should_get_teacher_introduction() {
+        final String teacherIntroduction = "There is a teacher who will introduce himself like this: My name is Tom. I am 18 years old. Teaching for the future of world.";
+        Teacher teacher = new Teacher();
+        teacher.setAge(18);
+        teacher.setName("Tom");
+        assertEquals(teacherIntroduction, teacher.introduce());
     }
 }
