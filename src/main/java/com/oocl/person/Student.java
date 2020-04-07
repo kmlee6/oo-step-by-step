b6ceb7db9f683830af97bef22bbb6d1c7bd5f6eb
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-07 15:51:14 +0800

    add class num field for student

--- src/main/java/com/oocl/person/Student.java
+++ src/main/java/com/oocl/person/Student.java
@@ -1,8 +1,18 @@
 package com.oocl.person;
 
-public class Student extends Person{
+public class Student extends Person {
+    private int classNumber;
+
+    public void setClassNumber(int classNumber) {
+        this.classNumber = classNumber;
+    }
+
+    public int getClassNumber() {
+        return this.classNumber;
+    }
+
     @Override
     public String introduce() {
-        return String.format("My name is %s. I am %d years old. Coding for the glory of OOCL", this.getName(), this.getAge());
+        return String.format("My name is %s. I am %d years old. I am a student of class %d. Coding for the glory of OOCL.", this.getName(), this.getAge(), this.getClassNumber());
     }
 }
--- src/test/java/com/oocl/person/StudentTest.java
+++ src/test/java/com/oocl/person/StudentTest.java
@@ -8,9 +8,10 @@ public class StudentTest {
 
     @Test
     public void should_get_student_introduction() {
-        final String studentIntroduction = "My name is Tom. I am 18 years old. Coding for the glory of OOCL";
+        final String studentIntroduction = "My name is Tom. I am 18 years old. I am a student of class 2. Coding for the glory of OOCL.";
         Student student = new Student();
         student.setAge(18);
+        student.setClassNumber(2);
         student.setName("Tom");
         assertEquals(studentIntroduction, student.introduce());
     }
