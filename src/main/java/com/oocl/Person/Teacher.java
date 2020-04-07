9e8b243ab2ec82dfcae9c1f08b8835a8931139ee
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-07 14:43:01 +0800

    completed Teacher Class

--- src/main/java/com/oocl/Person/Teacher.java
+++ src/main/java/com/oocl/Person/Teacher.java
@@ -1,4 +1,9 @@
 package com.oocl.Person;
 
 public class Teacher extends Person {
+    @Override
+    public String introduce() {
+        return String.format("There is a teacher who will introduce himself like this: " +
+                "My name is %s. I am %d years old. Teaching for the future of world.", this.getName(), this.getAge());
+    }
 }
